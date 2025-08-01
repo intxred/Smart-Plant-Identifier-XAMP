fb65118c48c6a5bde351fed905adbac89e0d9ca5
intxred <redbalane14@gmail.com>
2025-08-02 00:01:12 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "plant_identifier_db"; // ✅ must match exactly in phpMyAdmin
+$database = 
 
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
